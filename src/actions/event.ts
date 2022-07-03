702d05c0acd06248db7cca52968ef17c5931c163
Ryan Soury <ryan@webdoodle.com.au>
2022-07-03 19:36:18 +1000

    solve types for framebus param

--- src/actions/event.ts
+++ src/actions/event.ts
@@ -29,86 +29,87 @@ const action = async (conversion: Conversion) => {
 	}
 
 	// Received the cid (Conversion ID)
-	bus.on(
-		"conversion",
-		async ({ token, visitorId }: { token: string; visitorId?: string }) => {
-			if (!token) {
-				console.log(`[USHER] No token received from a valid referral`);
-				return;
-			}
-			if (!conversion.id || !conversion.chain || !conversion.eventId) {
-				console.log(
-					`[USHER] Campaign 'id', 'chain' and 'eventId' must be specified to track a conversion`
-				);
-				return;
-			}
+	bus.on("conversion", async (busParams) => {
+		const { token, visitorId } = busParams as {
+			token: string;
+			visitorId?: string;
+		};
 
-			// Authenticate a DID
-			let entropy = new Uint8Array();
-			if (visitorId) {
-				const hash = new Sha256();
-				hash.update(visitorId);
-				entropy = await hash.digest();
-			} else {
-				entropy = fromString([cuid(), randomString(6)].join("_"));
-			}
-
-			const did = new DID({
-				// Get the DID provider from the 3ID Connect instance
-				provider: new Ed25519Provider(entropy),
-				resolver: getKeyResolver()
-			});
-			await did.authenticate();
-
-			// Produce the DID Auth Token
-			const nonce = randomString(32);
-			const sig = await did.createJWS(nonce, { did: did.id });
-			const raw = [did.id, sig];
-			const authToken = Base64.encode(JSON.stringify(raw));
-
-			const request = ky.create({
-				prefixUrl: apiUrl,
-				headers: {
-					Authorization: `Bearer ${authToken}`
-				}
-			});
-
-			// Start the conversion using the referral token
-			const response: { success: boolean; data: { code: string } } =
-				await request
-					.get(
-						`conversions?id=${conversion.id}&chain=${conversion.chain}&token=${token}`
-					)
-					.json();
-			if (!response.success) {
-				console.log(
-					"[USHER] Failed to convert start conversion using referral token"
-				);
-				return;
-			}
-
-			const { code } = response.data;
-
-			// This part will eventually be replaced with a syndication of data to the decentralised DPK network
-			const saveResponse: { success: boolean; message?: string } = await request
-				.post("conversions", {
-					json: {
-						code,
-						...conversion
-					}
-				})
-				.json();
-
-			if (!saveResponse.success) {
-				console.log(
-					`[USHER] Could not save conversion: ${saveResponse.message || ""}`
-				);
-				return;
-			}
-
-			bus.emit("consume");
+		if (!token) {
+			console.log(`[USHER] No token received from a valid referral`);
+			return;
 		}
-	);
+		if (!conversion.id || !conversion.chain || !conversion.eventId) {
+			console.log(
+				`[USHER] Campaign 'id', 'chain' and 'eventId' must be specified to track a conversion`
+			);
+			return;
+		}
+
+		// Authenticate a DID
+		let entropy = new Uint8Array();
+		if (visitorId) {
+			const hash = new Sha256();
+			hash.update(visitorId);
+			entropy = await hash.digest();
+		} else {
+			entropy = fromString([cuid(), randomString(6)].join("_"));
+		}
+
+		const did = new DID({
+			// Get the DID provider from the 3ID Connect instance
+			provider: new Ed25519Provider(entropy),
+			resolver: getKeyResolver()
+		});
+		await did.authenticate();
+
+		// Produce the DID Auth Token
+		const nonce = randomString(32);
+		const sig = await did.createJWS(nonce, { did: did.id });
+		const raw = [did.id, sig];
+		const authToken = Base64.encode(JSON.stringify(raw));
+
+		const request = ky.create({
+			prefixUrl: apiUrl,
+			headers: {
+				Authorization: `Bearer ${authToken}`
+			}
+		});
+
+		// Start the conversion using the referral token
+		const response: { success: boolean; data: { code: string } } = await request
+			.get(
+				`conversions?id=${conversion.id}&chain=${conversion.chain}&token=${token}`
+			)
+			.json();
+		if (!response.success) {
+			console.log(
+				"[USHER] Failed to convert start conversion using referral token"
+			);
+			return;
+		}
+
+		const { code } = response.data;
+
+		// This part will eventually be replaced with a syndication of data to the decentralised DPK network
+		const saveResponse: { success: boolean; message?: string } = await request
+			.post("conversions", {
+				json: {
+					code,
+					...conversion
+				}
+			})
+			.json();
+
+		if (!saveResponse.success) {
+			console.log(
+				`[USHER] Could not save conversion: ${saveResponse.message || ""}`
+			);
+			return;
+		}
+
+		bus.emit("consume");
+	});
 
 	bus.emit("ping");
 };
