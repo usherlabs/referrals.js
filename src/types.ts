6a6bf87bb92426caa8e150d88c339b5f7327561b
Ryan Soury <ryan@webdoodle.com.au>
2022-07-16 14:51:38 +1000

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -19,13 +19,3 @@ export type Conversion = CampaignReference & {
 	metadata?: Record<string, string | number | boolean>;
 	commit?: number;
 };
-
-export type ConversionResponse = {
-	did: string;
-	response: Record<string, string | number | boolean>;
-	conversion: Conversion;
-};
-
-export type ConversionCallback = (response: ConversionResponse) => void;
-
-export type EventParams = Conversion | ConversionCallback | (() => void) | null;
